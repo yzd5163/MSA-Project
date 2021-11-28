d1ce5fca01783a959d62d212d8250885d3af1180
yzd5163 <72231116+yzd5163@users.noreply.github.com>
2021-11-28 17:41:27 -0500

    Add files via upload

---
+++ globalvariables.py
@@ -0,0 +1,2 @@
+MSA_END_POINT = 'https://southeastasia.api.cognitive.microsoft.com/customvision/v3.0/Prediction/3e9a0969-0716-43fc-977b-e48f60ac239d/detect/iterations/Iteration4/image'
+MSA_PREDICTION_KEY = 'cdb7442638f148e0ab9644d9d30ce435'
